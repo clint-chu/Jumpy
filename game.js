1d1a5d729bc2e82ab5c95da2cf738d18c6104536
clintchu <clintchu@outlook.com>
2020-10-19 21:48:11 -0700

    Create start()

--- game.js
+++ game.js
@@ -1,11 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid');
     const jumper = document.createElement('div');
+    let jumperLeft = 50;
+    let jumperBottom = 150;
+    let isGameOver = false;
 
     function createJumper() {
         grid.appendChild(jumper);
-        jumper.classList.add('jumper')
+        jumper.classList.add('jumper');
+        jumper.style.left = jumperLeft + 'px';
+        jumper.style.bottom = jumperBottom + 'px';
     };
 
-    createJumper();
+    function createPlatform() {
+        
+    };
+
+    function start() {
+        if (!isGameOver) {
+            createJumper();
+            createPlatforms();
+        };
+    };
+
+    start();
+
 });
