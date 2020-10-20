2bbb5c2f99282a37cd28eb2216dca1d42f7645f2
clintchu <clintchu@outlook.com>
2020-10-19 21:38:03 -0700

    Create jumper

--- game.js
+++ game.js
@@ -2,4 +2,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid');
     const jumper = document.createElement('div');
 
+    function createJumper() {
+        grid.appendChild(jumper);
+        jumper.classList.add('jumper')
+    };
+
+    createJumper();
 });
