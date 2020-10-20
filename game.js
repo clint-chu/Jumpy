8ef5a5c0655a3f089425b4438d162e02f5cd3be0
clintchu <clintchu@outlook.com>
2020-10-19 22:12:54 -0700

    Add platform class and function

--- game.js
+++ game.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const jumper = document.createElement('div');
     let jumperLeft = 50;
     let jumperBottom = 150;
+    let platformCount = 5;
     let isGameOver = false;
 
     function createJumper() {
@@ -12,8 +13,26 @@ document.addEventListener('DOMContentLoaded', () => {
         jumper.style.bottom = jumperBottom + 'px';
     };
 
+    class Platform {
+        constructor(newPlatform) {
+            this.bottom = newPlatformBottom;
+            this.left = Math.random() * 315;
+            this.visual = document.createElement('div');
+
+            const visual = this.visual;
+            visual.classList.add('platform');
+            visual.style.left = this.left + 'px';
+            visual.style.bottom = this.bottom + 'px';
+            grid.appendChild(visual);
+        };
+    };
+
     function createPlatform() {
-        
+        for (let i = 0; i < platformCount; i++) {
+            let platformGap = 600 / platformCount;
+            let newPlatformBottom = 100 + i * platformGap;
+            let newPlatform = new platformGap(newPlatformBottom);
+        };
     };
 
     function start() {
